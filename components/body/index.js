7e0f79aba12e4d27d0f45cc1d6131559b6c83620
Jesus Puentes <jopt98@gmail.com>
2022-05-31 14:11:24 -0500

    Masonry works!

--- components/body/index.js
+++ components/body/index.js
@@ -5,40 +5,17 @@ export default function Body(props) {
     return (
         <main className={styles.Body}>
             <div className={`${styles.Image_Container}`}>
+                <div className={styles.Overlay}>
+                </div>
+                <button className={styles.Delete_Button}>
+                    Delete
+                </button>
                 <img src='https://www.gettyimages.com.mx/gi-resources/images/500px/983703508.jpg' />
-            </div>
-            <div className={`${styles.Image_Container}`}>
-                <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/mulan_2.jpg' />
-            </div>
-            <div className={`${styles.Image_Container}`}>
-                <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/cinderella.jpg' />
-            </div>
-            <div className={`${styles.Image_Container}`}>
-                <img src='https://img.rawpixel.com/private/static/images/website/2022-05/px142077-image-kwvvvktc.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=0cfd8aa6e66ad55612f900d479a9d0dehttps://img.rawpixel.com/private/static/images/website/2022-05/px142077-image-kwvvvktc.jpg' />
-            </div>
-            <div className={`${styles.Image_Container}`}>
-                <img src='https://www.gettyimages.com.mx/gi-resources/images/500px/983703508.jpg' />
-            </div>
-            <div className={`${styles.Image_Container}`}>
-                <img src='https://www.gettyimages.com.mx/gi-resources/images/500px/983703508.jpg' />
-            </div>
-            <div className={`${styles.Image_Container}`}>
-                <img src='https://www.gettyimages.com.mx/gi-resources/images/500px/983703508.jpg' />
-            </div>
-            <div className={`${styles.Image_Container}`}>
-                <img src='https://www.gettyimages.com.mx/gi-resources/images/500px/983703508.jpg' />
-            </div>
-            <div className={`${styles.Image_Container}`}>
-                <img src='https://www.gettyimages.com.mx/gi-resources/images/500px/983703508.jpg' />
-            </div>
-            <div className={`${styles.Image_Container}`}>
-                <img src='https://www.gettyimages.com.mx/gi-resources/images/500px/983703508.jpg' />
-            </div>
-            <div className={`${styles.Image_Container}`}>
-                <img src='https://www.gettyimages.com.mx/gi-resources/images/500px/983703508.jpg' />
-            </div>
-            <div className={`${styles.Image_Container}`}>
-                <img src='https://www.gettyimages.com.mx/gi-resources/images/500px/983703508.jpg' />
+                <div className={styles.Image_Desc}>
+                    <p>
+                        Morbi consequat lorem ipsuma ada dasdasdasdasdasdadas
+                    </p>
+                </div>
             </div>
         </main>
     )
