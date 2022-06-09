efcda3e2cc4d648091b6c4a895a9e5edfdfafa56
Jesus Puentes <jopt98@gmail.com>
2022-06-09 17:19:11 -0500

    Popup

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Body from '../components/body'
 import Header from '../components/header'
+import Popup from '../components/popup'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -15,6 +16,7 @@ export default function Home() {
 
       <Header />
       <Body />
+      <Popup />
     </div>
   )
 }
